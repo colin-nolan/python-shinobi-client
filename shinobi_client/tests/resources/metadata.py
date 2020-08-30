ca07f71a734e3f273dcc410cd501604bf8bc318d
Colin Nolan <cn580@alumni.york.ac.uk>
2020-08-30 01:41:07 +0100

    Adds tests to check intended functionality.

--- shinobi_client/tests/resources/metadata.py
+++ shinobi_client/tests/resources/metadata.py
@@ -1,16 +1,18 @@
 import json
 import os
-from typing import Dict
+from typing import Dict, Any
 
-RESOURCE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+_RESOURCE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
+_MONITORS_DIRECTORY = os.path.join(_RESOURCE_DIRECTORY, "monitors")
 EXAMPLE_MONITOR_LOCATIONS = {
-    1: os.path.join(RESOURCE_DIRECTORY, "example-monitor-1.json"),
-    2: os.path.join(RESOURCE_DIRECTORY, "example-monitor-2.json")
+    1: os.path.join(_MONITORS_DIRECTORY, "example-1.json"),
+    2: os.path.join(_MONITORS_DIRECTORY, "example-2.json"),
+    3: os.path.join(_MONITORS_DIRECTORY, "details-as-json.json"),
 }
 
 
-def get_monitor_configuration(example_monitor_id: int = 1) -> Dict:
+def get_monitor_configuration(example_monitor_id: Any = 1) -> Dict:
     """
     Gets the example monitor configuration with the given ID (or first monitor if no ID is given).
     :param example_monitor_id: ID of monitor to load
